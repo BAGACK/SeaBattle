d8381ccc3887815e6ad86a05b8978c491ad0e774
mepeisen <mep_eisen@web.de>
2016-06-25 20:42:29 +0200

    rework config strings

--- src/main/java/com/comze_instancelabs/mgseabattle/Main.java
+++ src/main/java/com/comze_instancelabs/mgseabattle/Main.java
@@ -22,6 +22,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
 
 import com.comze_instancelabs.minigamesapi.Arena;
+import com.comze_instancelabs.minigamesapi.ArenaConfigStrings;
 import com.comze_instancelabs.minigamesapi.ArenaSetup;
 import com.comze_instancelabs.minigamesapi.ArenaState;
 import com.comze_instancelabs.minigamesapi.MinigamesAPI;
@@ -98,7 +99,7 @@ public class Main extends JavaPlugin implements Listener {
 		if (!config.isSet("arenas")) {
 			return ret;
 		}
-		for (String arena : config.getConfigurationSection("arenas.").getKeys(false)) {
+		for (String arena : config.getConfigurationSection(ArenaConfigStrings.ARENAS_PREFIX).getKeys(false)) {
 			if (Validator.isArenaValid(plugin, arena, cf.getConfig())) {
 				ret.add(initArena(arena));
 			}
